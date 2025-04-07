e065c707d06c26c56c6e7a6bdebba91a341d3037
wuquan <17324432872@163.com>
2025-04-07 15:29:40 +0800

    添加验证过的地址

--- contracts/ERC721/IERC165.sol
+++ contracts/ERC721/IERC165.sol
@@ -1,13 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-/**
- * @dev ERC165标准接口, 详见
- * https://eips.ethereum.org/EIPS/eip-165[EIP].
- *
- * 合约可以声明支持的接口，供其他合约检查
- *
-
 interface IERC165 {
     function supportsInterface(bytes4 interfaceId) external view returns (bool);
 }
